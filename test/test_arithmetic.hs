112377230fb029043e4ca20d0b2ba1ae21143d5d
Bogdan Opanchuk <bogdan@lair.local>
2012-01-18 17:42:58 +1100

    test/test_arithmetic: updated tests, added test for zero result

--- test/test_arithmetic.hs
+++ test/test_arithmetic.hs
@@ -1,8 +1,9 @@
 import qualified Wigner.Symbols as S
 import qualified Wigner.DefineOpExpr as DO
 import qualified Wigner.DefineFuncExpr as DF
-import Wigner.Complex
 import Wigner.Texable
+
+import Data.Ratio
 
 import Test.Framework (defaultMain, testGroup)
 import Test.Framework.Providers.HUnit
@@ -12,20 +13,29 @@ a1 = DO.operatorIx S.a [S.ix_1]
 a2 = DO.operatorIx S.a [S.ix_2]
 b1 = DO.operatorIx S.b [S.ix_1]
 b2 = DO.operatorIx S.b [S.ix_2]
+alpha = DO.constant S.alpha
+beta = DO.constant S.beta
 
 test_group_terms = (expr1 + expr2) @?= result where
-    expr1 = a1 * 2 + a2 * 3
-    expr2 = b1 * 4 + a1 * 2
-    result = b1 * 4 + a2 * 3 + a1 * 4
+    expr1 = alpha * a1 * (2 - DO.i) + a2 * 3
+    expr2 = b1 * 4 + alpha * a1 * (1 + 2 * DO.i)
+    result = b1 * 4 + a2 * 3 + alpha * a1 * (3 + DO.i)
 
-test_remove_zero_terms = (expr1 + expr2) @?= result where
-    expr1 = a1 * 2 + a2 * 3
-    expr2 = b1 * 4 - a1 * 2
-    result = b1 * 4 + a2 * 3
+test_remove_zero_terms = (expr1 + expr2 + expr3) @?= result where
+    expr1 = beta * a1 * (fromRational (2 % 3) - DO.i / 2) + a2 * 3
+    expr2 = b1 * 4 - beta * a1 * (fromRational (-2 % 3) + DO.i / 2)
+    expr3 = b2 * 4 - beta * a1 * (fromRational (4 % 3) - DO.i) - 2 * alpha * a1
+    result = b1 * 4 + a2 * 3 + b2 * 4 - 2 * alpha * a1
+
+test_zero_result = (expr1 + expr2) @?= 0 where
+    expr1 = 2 * a1 - 2 * b1
+    expr2 = 2 * b1 - 2 * a1
+
 
 tg_addition = testGroup "Addition" [
         testCase "group_terms" test_group_terms,
-        testCase "remove_zero_terms" test_remove_zero_terms
+        testCase "remove_zero_terms" test_remove_zero_terms,
+        testCase "zero_result" test_zero_result
     ]
 tests = [tg_addition]
 
