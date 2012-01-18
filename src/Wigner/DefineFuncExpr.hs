95342231117ec5d4d81e57f00dfabf730c683571
Bogdan Opanchuk <bogdan@lair.local>
2012-01-18 20:28:20 +1100

    src/Wigner/DefineFuncExpr: added helpers for differentials

--- src/Wigner/DefineFuncExpr.hs
+++ src/Wigner/DefineFuncExpr.hs
@@ -1,19 +1,27 @@
 module Wigner.DefineFuncExpr(
     functionIx, function, constantIx, constant,
+    differentialFuncIx, differentialIx, differential,
     fromOpExpr, i) where
 
     import Wigner.Complex
     import Wigner.Expression
     import qualified Data.Map as M
 
-    functionIx s i v = Sum $ M.singleton func_term 1 where
+    makeExpr factor = Sum $ M.singleton (FuncTerm [factor]) 1
+
+    functionIx s i v = makeExpr func_factor where
         func = Func $ Element s i v
         func_factor = FuncProduct $ M.singleton func 1
-        func_term = FuncTerm [func_factor]
     function s = functionIx s []
 
     constantIx s i = functionIx s i []
     constant s = functionIx s [] []
+
+    differentialFuncIx s i v = makeExpr func_factor where
+        diff = Diff $ Element s i v
+        func_factor = DiffProduct $ M.singleton diff 1
+    differentialIx s i = differentialFuncIx s i []
+    differential s = differentialFuncIx s [] []
 
     fromOpTerm (OpTerm fs Nothing)
         | M.null fs = FuncTerm []
