d7df5feab592dfe82523c66b5bd7d8bd78f08a08
Bogdan Opanchuk <bogdan@lair.local>
2012-01-19 11:57:50 +1100

    test/test_operators: added two tests for symmetric transformations

--- test/test_operators.hs
+++ test/test_operators.hs
@@ -10,6 +10,7 @@ import Test.Framework.Providers.HUnit
 import Test.HUnit
 
 a1 = DO.operatorIx S.a [S.ix_1]
+da1 = dagger a1
 a2 = DO.operatorIx S.a [S.ix_2]
 b1 = DO.operatorIx S.b [S.ix_1]
 b2 = DO.operatorIx S.b [S.ix_2]
@@ -17,15 +18,24 @@ alpha = DO.constant S.alpha
 beta = DO.constant S.beta
 
 
-test_symmetric_to_normal = (toNormalProduct (asSymmetricProduct expr)) @?= result where
-    da1 = dagger a1
+test_define_symmetric = expr1 @?= expr2 where
+    expr1 = DO.symmetric $ da1 ^ 2 * a1 ^ 2
+    expr2 = DO.symmetric $ a1 ^ 2 * da1 ^ 2
+
+test_normal_to_normal = (toNormalProduct expr) @?= result where
     expr = da1 ^ 2 * a1 ^ 2
+    result = da1 ^ 2 * a1 ^ 2
+
+test_symmetric_to_normal = (toNormalProduct expr) @?= result where
+    expr = DO.symmetric $ da1 ^ 2 * a1 ^ 2
     result = (da1 ^ 2 * a1 ^ 2 + da1 * a1 * da1 * a1 + da1 * a1 ^ 2 * da1 +
         a1 * da1 ^ 2 * a1 + a1 * da1 * a1 * da1 + a1 ^ 2 * da1 ^ 2) / 6
 
 
 
 tg_operators = testGroup "Operator Algebra" [
+        testCase "define_symmetric" test_define_symmetric,
+        testCase "normal_to_normal" test_normal_to_normal,
         testCase "symmetric_to_normal" test_symmetric_to_normal
     ]
 tests = [tg_operators]
