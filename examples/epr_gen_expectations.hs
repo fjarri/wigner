1546c1dcdee55b398a28700e5f5e1d4bc0ff157d
Bogdan Opanchuk <bogdan@opanchuk.net>
2012-01-24 17:24:23 +1100

    examples: committed actual script I used for EPR

--- examples/epr_gen_expectations.hs
+++ examples/epr_gen_expectations.hs
@@ -32,6 +32,11 @@ sin_t = D.constant $ S.symbol "\\sin(\\theta)"
 
 ca2a1 = (dagger a2) * a1
 
+jAX = ((dagger a2) * a1 * e_idt + (dagger a1) * a2 * e_m_idt) / 2
+jAY = ((dagger a2) * a1 * e_idt - (dagger a1) * a2 * e_m_idt) / (2 * D.i)
+jAZ = ((dagger a2) * a2 - (dagger a1) * a1) / 2
+jAT = cos_t * jAZ + sin_t * jAX
+
 jCX = (dagger c2 * c1 * e_idt + dagger c1 * c2 * e_m_idt) / 2
 jCY = (dagger c2 * c1 * e_idt - dagger c1 * c2 * e_m_idt) / (2 * D.i)
 jCZ = (dagger c2 * c2 - dagger c1 * c1) / 2
@@ -53,6 +58,11 @@ constants = [
 exprs = [
     Result ComplexValue (eval $ E.expectation ca2a1) "exp_ca2a1",
     UserCalculation "d_theta",
+    Result RealValue (eval $ E.expectation jAY) "exp_jAY",
+	Result RealValue (eval $ E.variance jAZ jAX) "var_jAZ_jAX",
+	Result RealValue (eval $ E.deltaSquared jAZ) "d2_jAZ",
+	Result RealValue (eval $ E.deltaSquared jAX) "d2_jAX",
+	Lambda RealValue (eval $ E.deltaSquared jAT) "d2_jAT" ["theta"],
     Lambda RealValue (eval $ E.deltaSquared jDT) "d2_jDT" ["theta"],
     Lambda RealValue (eval $ E.variance jCT jDT) "var_jCTjDT" ["theta"],
     Result RealValue (eval $ E.expectation jCY) "exp_jCY",
