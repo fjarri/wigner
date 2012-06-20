220fd5eaddd6881fc7b5941ed8ee105a263a2719
Bogdan Opanchuk <bogdan@opanchuk.net>
2012-06-20 15:57:54 +1000

    test/TestTrace: added test for arithmetic operations of 2by2 expression matrices

--- test/TestTrace.hs
+++ test/TestTrace.hs
@@ -3,6 +3,8 @@ module TestTrace(test_group) where
 import qualified Wigner.Symbols as S
 import qualified Wigner.DefineExpression as D
 import qualified Wigner.Trace as T
+import Wigner.Expression
+import Wigner.Texable
 
 import Data.Ratio
 
@@ -18,6 +20,19 @@ test_trace = T.trace expr @?= result where
     expr = D.expr2by2 (2 * g * h) h g (h * g)
     result = 2 * T.trace (g * h) + T.trace (h * g)
 
+test_2by2_operations = (showTex expr) @?= (showTex result) where
+    m1 = D.expr2by2 g h (-h) g
+    m2 = transpose m1
+    tg = transpose g
+    th = transpose h
+    expr = m1 * m2 + 2 * m1
+    result = D.expr2by2
+        (g * tg + h * th + 2 * g)
+        (h * tg - g * th + 2 * h)
+        (g * th - h * tg - 2 * h)
+        (g * tg + h * th + 2 * g)
+
 test_group = testGroup "Trace" [
-    testCase "trace" test_trace
+    testCase "trace" test_trace,
+    testCase "2by2_operations" test_2by2_operations
     ]
